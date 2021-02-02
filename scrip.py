4f092999544f264bf851957ec97ecec23205633b
AndresGatjens <60201579+AndresGatjens@users.noreply.github.com>
2021-02-01 19:00:17 -0600

    Add files via upload
    mi peso en varios planetas

---
+++ scrip.py
@@ -0,0 +1,95 @@
+# ....Tarea de mister sensie Loo.....#
+__________________________________________
+
+#____Peso del Usuario en cada planeta____#
+
+
+#............. Peso Júpiter ............# 
+
+pesoDelUsuario = float(input("Introdusca su peso en (Kg): "))
+
+pesoTotalEnJúpiter = pesoDelUsuario * 2.55
+
+pesoDeUsuarioEnJúpiter = "su peso es : " + str(pesoTotalEnJúpiter) + "kg en Júpiter"
+
+print(pesoDeUsuarioEnJúpiter)
+
+#............. Peso en Venus ...........# 
+
+pesoDelUsuario = float(input("Introdusca su peso en (Kg): "))
+
+pesoTotalEnVenus = pesoDelUsuario * 0.87
+
+pesoDeUsuarioEnVenus = "su peso es : " + str(pesoTotalEnVenus) + "kg en Venus"
+
+print(pesoDeUsuarioEnVenus)
+
+#.............. Peso en Urano ..........# 
+
+pesoDelUsuario = float(input("Introdusca su peso en (Kg): "))
+
+pesoTotalEnUrano = pesoDelUsuario * 0.99
+
+pesoDeUsuarioEnUrano = "su peso es : " + str(pesoTotalEnUrano) + "kg en Urano"
+
+print(pesoDeUsuarioEnUrano)
+
+#.............. Peso en Marte ..........# 
+
+pesoDelUsuario = float(input("Introdusca su peso en (Kg): "))
+
+pesoTotalEnMarte = pesoDelUsuario * 0.38
+
+pesoDeUsuarioEnMarte = "su peso es : " + str(pesoTotalEnMarte) + "kg en Marte"
+
+print(pesoDeUsuarioEnMarte)
+
+#............. Peso en Mercurio .........# 
+
+pesoDelUsuario = float(input("Introdusca su peso en (Kg): "))
+
+pesoTotalEnMercurio = pesoDelUsuario * 0.39
+
+pesoDeUsuarioEnMercurio = "su peso es : " + str(pesoTotalEnMercurio) + "kg en Mercurio"
+
+print(pesoDeUsuarioEnMercurio)
+
+#.............Peso en Saturno ...........# 
+
+pesoDelUsuario = float(input("Introdusca su peso en (Kg): "))
+
+pesoTotalEnSaturno = pesoDelUsuario * 0.93
+
+pesoDeUsuarioEnSaturno = "su peso es : " + str(pesoTotalEnSaturno) + "kg en Saturno"
+
+print(pesoDeUsuarioEnSaturno)
+
+#............. Peso en Neptuno ...........# 
+
+pesoDelUsuario = float(input("Introdusca su peso en (Kg): "))
+
+pesoTotalEnNeptuno = pesoDelUsuario * 1.38
+
+pesoDelUsuarioEnNeptuno = "su peso es : " + str(pesoTotalEnNeptuno) + "kg en Neptuno"
+
+print(pesoDelUsuarioEnNeptuno)
+
+#............. Peso en la Tierra .........# 
+
+pesoDelUsuario = float(input("Introdusca su peso en (Kg): "))
+
+pesoTotalEnTierra = pesoDelUsuario * 1
+
+pesoDelUsuarioEnLaTierra = "su peso es : " + str(pesoTotalEnTierra) + "kg en la Tierra"
+
+print(pesoDelUsuarioEnLaTierra)
+
+#........... Peso en la LUNA ...........# 
+
+pesoDelUsuario = float(input("Introdusca su peso en (Kg): "))
+
+pesoTotalEnLaLuna = pesoDelUsuario * 0.17
+
+pesoDeUsuarioEnLaLuna = "su peso es : " + str(pesoTotalEnLaLuna) + "kg en la luna"
+
+print(pesoDelUsuarioEnLaLuna)
